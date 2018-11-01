0ae07cc951d232816eaf51fcd1e8cf5e819aca7d
Ross M <37253938+RossMdevs@users.noreply.github.com>
2018-11-01 17:25:08 -0400

    Created Stand-Alone File

---
+++ src/main.js
@@ -0,0 +1,22 @@
+// Simple Documentation Launcher
+// Coded by https://github.com/RossMdevs 2018 
+
+// Loads OPN
+const opn = require('opn')
+
+
+
+
+//Opens documentation after initiation
+
+
+opn('').then(() => {
+    // Checks if docs closed
+});
+ 
+
+
+// console logging
+console.log("----------------")
+console.log("Documentation Loaded")
+console.log("Docs won't load if a TXT file isn't found with the docs in it")
